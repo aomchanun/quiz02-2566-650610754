4a72bf92ecaeae22bd783e981bde5cee1cda0067
aomchanun <omsinjah@gmail.com>
2023-08-10 15:36:51 +0700

    edited

--- src/app/page.js
+++ src/app/page.js
@@ -71,7 +71,7 @@ export default function HomePage() {
         </div> */}
         {comments.map((com) => (
           <Comment
-            key={com.replies}
+            key={com.username}
             userImagePath={com.userImagePath}
             username={com.username}
             commentText={com.commentText}
